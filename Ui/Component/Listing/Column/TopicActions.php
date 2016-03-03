baac509d6ba971d23594635f98ae8d0188d940a8
Ammar <ammar@shopgo.me>
2016-03-03 15:33:00 +0200

    Add UI component topic actions class

---
+++ Ui/Component/Listing/Column/TopicActions.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Copyright © 2015 ShopGo. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace ShopGo\AmazonSns\Ui\Component\Listing\Column;
+
+use Magento\Framework\UrlInterface;
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Framework\View\Element\UiComponentFactory;
+use Magento\Ui\Component\Listing\Columns\Column;
+
+/**
+ * Class TopicActions
+ */
+class TopicActions extends Column
+{
+    /**
+     * Url path
+     */
+    const URL_PATH_EDIT   = 'amazon/sns_topic/edit';
+    const URL_PATH_DELETE = 'amazon/sns_topic/delete';
+
+    /**
+     * @var UrlInterface
+     */
+    protected $urlBuilder;
+
+    /**
+     * Constructor
+     *
+     * @param ContextInterface $context
+     * @param UiComponentFactory $uiComponentFactory
+     * @param UrlInterface $urlBuilder
+     * @param array $components
+     * @param array $data
+     */
+    public function __construct(
+        ContextInterface $context,
+        UiComponentFactory $uiComponentFactory,
+        UrlInterface $urlBuilder,
+        array $components = [],
+        array $data = []
+    ) {
+        $this->urlBuilder = $urlBuilder;
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+    }
+
+    /**
+     * Prepare Data Source
+     *
+     * @param array $dataSource
+     * @return array
+     */
+    public function prepareDataSource(array $dataSource)
+    {
+        if (isset($dataSource['data']['items'])) {
+            foreach ($dataSource['data']['items'] as & $item) {
+                if (isset($item['topic_id'])) {
+                    $item[$this->getData('name')] = [
+                        'delete' => [
+                            'href' => $this->urlBuilder->getUrl(
+                                static::URL_PATH_DELETE,
+                                [
+                                    'topic_id' => $item['topic_id']
+                                ]
+                            ),
+                            'label' => __('Delete'),
+                            'confirm' => [
+                                'title' => __('Delete "${ $.$data.name }"'),
+                                'message' => __('Are you sure you want to delete a "${ $.$data.name }" record?')
+                            ]
+                        ]
+                    ];
+                }
+            }
+        }
+
+        return $dataSource;
+    }
+}
